430aad74b7df89d51788e9e1ee9d5b4179b401c3
Ravuthz <ravuthz@gmail.com>
2026-04-24 20:51:55 +0700

    Add gitingore

--- src/types/geojson.ts
+++
@@ -1,7 +0,0 @@
-import type { Feature, FeatureCollection, Geometry } from 'geojson'
-
-export type DrawFeature = Feature<Geometry, Record<string, unknown>>
-export type DrawFeatureCollection = FeatureCollection<Geometry, Record<string, unknown>>
-
-export type DrawMode = 'select' | 'point' | 'line_string' | 'polygon' | 'rectangle'
-export type ToggleStyle = 'icon-only' | 'icon-label' | 'sidebar'
